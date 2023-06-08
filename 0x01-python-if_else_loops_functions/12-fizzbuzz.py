4b1cde1c129d670ea488fa508841982d19d4f372
root <Beethoven Kasompe>
2023-06-07 17:56:08 -0700

    Tasks9-12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        elif i % 3 == 0:
+        if i % 3 == 0:
             print("Fizz ", end="")
         elif i % 5 == 0:
             print("Buzz ", end="")

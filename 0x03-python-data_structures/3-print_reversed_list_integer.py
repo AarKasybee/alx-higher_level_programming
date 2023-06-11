181f05797f5010dad1578e82717357268f556895
root <Beethoven Kasompe>
2023-06-11 16:50:47 -0700

    reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    idx = len(my_list) - 1
-    m = []
     if my_list:
+        idx = len(my_list) - 1
+        m = []
         for i in range(len(my_list)):
             m.append(my_list[idx])
             print("{:d}".format(m[i]))
